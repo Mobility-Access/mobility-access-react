a2148aa2cbed06fe693d9e53a65456b94c1bd22a
dgboss <darrengboss@gmail.com>
2021-10-13 09:04:29 -0700

    Update host for api requests

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
-export const ApiUrl = "http://127.0.0.1:5000/api";
-// export const ApiUrl = "http://178.128.225.253/api";
+// export const ApiUrl = "http://127.0.0.1:5000/api";
+export const ApiUrl = "http://walkrollmap.org/api";
 export const GeocoderUrl = "https://nominatim.openstreetmap.org/search";
 export const AmenityUrl = `${ApiUrl}/amenity`;
 export const HazardUrl = `${ApiUrl}/hazard`;
