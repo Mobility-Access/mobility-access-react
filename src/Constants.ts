09542983b0b525af93ae2b79aa389443d15808a9
dgboss <darrengboss@gmail.com>
2021-10-13 09:08:18 -0700

    https

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
 // export const ApiUrl = "http://127.0.0.1:5000/api";
-export const ApiUrl = "http://walkrollmap.org/api";
+export const ApiUrl = "https://walkrollmap.org/api";
 export const GeocoderUrl = "https://nominatim.openstreetmap.org/search";
 export const AmenityUrl = `${ApiUrl}/amenity`;
 export const HazardUrl = `${ApiUrl}/hazard`;
