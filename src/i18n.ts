715a9cfb4d3500cdb12dae6b4f088b2af8bb3303
dgboss <darrengboss@gmail.com>
2021-10-30 14:45:03 -0700

    Add french translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
@@ -9,13 +8,12 @@ i18n
     .use(Backend)
     // Detect user language and store user selected language
     // https://github.com/i18next/i18next-browser-languageDetector
-    .use(LanguageDetector)
     // Pass the i18n instance to react-i18next
     .use(initReactI18next)
     // Initialize i18next
     // https://www.i18next.com/overview/configuration-options
     .init({
-        fallbackLng: "en",
+        fallbackLng: "fr",
         debug: true,
         interpolation: {
             escapeValue: false,
