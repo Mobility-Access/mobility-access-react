b1b01bdb0bd7199e43c6f2d0032eb91badca435b
dgboss <darrengboss@gmail.com>
2022-01-11 20:38:44 -0800

    Testing Google Analytics on dev site

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+// export const ga4MeasurementId = "";
+
+// WalkRollMap dev
+export const ga4MeasurementId = "G-8WL7092ZDM"
+
+// WalkRollMap prod
+// export const ga4MeasurementId = "G-6RW75023YF"
+
+// OnMarcheOnRoule prod
+// export const ga4MeasurementId = "G-SP2WMN6EG5"
