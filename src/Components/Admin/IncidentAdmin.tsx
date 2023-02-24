b01dab1045de00c5237206327cfaccf7e3fcceeb
dgboss <darrengboss@gmail.com>
2023-02-24 15:29:25 -0800

    More console warnings

--- src/Components/Admin/IncidentAdmin.tsx
+++ src/Components/Admin/IncidentAdmin.tsx
@@ -208,7 +208,7 @@ const IncidentAdmin = () => {
                         { visibleRows.length > 0 && (
                             <TableBody>
                                 { visibleRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item) => (
-                                    <TableReportRow handleDelete={handleDelete} key={item.id} row={item.properties} />
+                                    <TableReportRow handleDelete={handleDelete} key={item.properties.id} row={item.properties} />
                                 ))}
                             </TableBody>
                         )}
