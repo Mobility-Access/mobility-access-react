7aac972e2ac26bec46d3197e6963f3c344628025
dgboss <darrengboss@gmail.com>
2024-05-02 08:10:15 -0700

    Paramerterize base URL for resources

--- src/Components/About/Resources.tsx
+++ src/Components/About/Resources.tsx
@@ -1,15 +1,17 @@
+const BaseUrl = import.meta.env.BASE_URL
+
 export const englishReports: { text: string, hyperlink: string }[] = [
     {
         text: "Summary report for the Capital Regional District (BC), March 31, 2024",
-        hyperlink: `http://159.203.33.14/static/CRD_WRM_Final_Report.pdf`
+        hyperlink: `${BaseUrl}/static/CRD_WRM_Final_Report.pdf`
     },
     {
         text: "Engagement and Promotion Summary, March 31, 2024",
-        hyperlink: "https://www.dropbox.com/scl/fi/urfpnwxzuimr7egflheck/WRM-Promotion-and-Engagement.pdf?rlkey=0gc3cuwrpfvd9e5pvdgeu3z89&st=sozxuxug&dl=0"
+        hyperlink: `${BaseUrl}/static/WRM_Promotion_And_Engagement.pdf`
     },
     {
         text: "Example Guided Walk, prepared by Envirocentre, November 2022",
-        hyperlink: "https://www.dropbox.com/scl/fi/q2arytfiwtlpm2wm3085g/Guided-Walk-summary.pdf?rlkey=c0cmlua4b5dc3u42dasxmyb0e&st=iseaslhr&dl=0"
+        hyperlink: `${BaseUrl}/static/Guided_Walk_Summary.pdf`
     }
 
 ];
@@ -17,6 +19,6 @@ export const englishReports: { text: string, hyperlink: string }[] = [
 export const frenchReports: { text: string, hyperlink: string }[] = [
     {
         text: "OnMarcheOnRoule au Québec, le 31 mars 2024",
-        hyperlink: "https://www.dropbox.com/scl/fi/vjnv4msam4kmr6q43y05q/Final-Quebec-OMOR.pdf?rlkey=ove0xad3ab39xx124fx6tc84k&st=s6lc40pg&dl=0"
+        hyperlink: `${BaseUrl}/static/Final_Quebec_OMOR.pdf`
     }
 ]
